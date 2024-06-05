de6d69d4e6ef2698c628972520489f4ff800ec4d
umid2401 <umamatraximov@gmail.com>
2024-06-05 22:45:34 +0500

    All changes

--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -1,39 +1,73 @@
-import { faCaretDown, faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import "..//Styles/Collection.scss"
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import {data} from "../data";
+import "..//Styles/Collection.scss";
 export default function Collection() {
-  
+  const [active, setActive] = useState(false);
+  const [products, setProducts] = useState(data);
+  const product=data;
+  const showButton = () => {
+    setActive(!active);
+  };
+  const dataFilerSeason = (season) =>{
+    const filterData = product.filter(item=>item.season===season);
+    setProducts(filterData);
+    setActive(!active);
+    console.log(filterData);
+    console.log(product)
+  }
+  // useEffect(()=>{
+  //   dataFilerSeason();
+  // },[])
   return (
-    <div className='collection'>
-       <div className="collection-wrapper">
+    <div className="collection">
+      <div className="collection-wrapper">
         <div className="left-box">
-          <div className="input-group">
-            <input type="text" placeholder='Search here' />
-            <div className="search" >
-              <img src="Images/icons8-search-24.png" alt="" />
-             
-            </div>
-          </div>
           <div className="btn-dropdown">
-            <button>
-              <span>Collection</span>
-              <FontAwesomeIcon
-                 
+            <div className="input">
+              <div className="input-group">
+                <input type="text" placeholder="Search here" />
+                <div className="search">
+                  <img src="Images/icons8-search-24.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="button">
+              <button onClick={showButton}>
+                <span>Collection</span>
+                <FontAwesomeIcon
+                  className={`icon ${active ? "active" : ""}`}
                   icon={faCaretRight}
                   style={{ color: "#FFD43B" }}
                 />
-            </button>
-            <div className="collection">
-              <span>Spring collection</span>
-              <span>Summer collection</span>
-              <span>Winter collection</span>
-              <span>Autumn collection</span>
+              </button>
+            </div>
+            <div className={`collection ${active ? "active" : ""}`}>
+              <span onClick={()=>dataFilerSeason("spring")} >Spring collection</span>
+              <span onClick={()=>dataFilerSeason("summer")} >Summer collection</span>
+              <span onClick={()=>dataFilerSeason("winter")} >Winter collection</span>
+              <span onClick={()=>dataFilerSeason("autumn")} >Autumn collection</span>
             </div>
           </div>
         </div>
-        <div className="right-box"></div>
-       </div>
+        <div className="right-box">
+          <div className="cards">
+              {products?(
+                products.map((card,index)=>(
+                  <div className="card" key={index}>
+                    <div className="img">
+                            <img src={card.imgUrl} alt={card.name} />
+                        </div>
+                        <p>{card.name}</p>
+                  </div>
+                ))
+              ):(
+                <div>Product not found</div>
+              )}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/data.js
+++ src/data.js
@@ -2,257 +2,308 @@
     {
       "imgUrl": "Images/diz-69-1-768x768.jpg",
       "name": "diz-69-1-768x768",
-      "season": "winter"
+      "season": "winter",
+      "price":"500",
     },
     {
       "imgUrl": "Images/diz-16-768x768.jpg",
       "name": "diz-16-768x768",
-      "season": "summer"
+      "season": "summer",
+      "price":"700",
     },
     {
       "imgUrl": "Images/diz-17-768x768.jpg",
       "name": "diz-17-768x768",
-      "season": "spring"
+      "season": "spring",
+      "price":"650",
     },
     {
       "imgUrl": "Images/diz-19-768x768.jpg",
       "name": "diz-19-768x768",
-      "season": "spring"
+      "season": "spring",
+      "price":"450",
     },
     {
       "imgUrl": "Images/diz-20-768x768.jpg",
       "name": "diz-20-768x768",
-      "season": "winter"
+      "season": "winter",
+      "price":"850",
     },
     {
       "imgUrl": "Images/diz-25-768x768.jpg",
       "name": "diz-25-768x768",
-      "season": "autumn"
+      "season": "autumn",
+      "price":"950",
     },
     {
       "imgUrl": "Images/diz-27-768x768.jpg",
       "name": "diz-27-768x768",
-      "season": "summer"
+      "season": "summer",
+      "price":"800",
     },
     {
       "imgUrl": "Images/diz-31-768x768.jpg",
       "name": "diz-31-768x768",
-      "season": "spring"
+      "season": "spring",
+      "price":"900",
     },
     {
       "imgUrl": "Images/diz-32-768x768.jpg",
       "name": "diz-32-768x768",
-      "season": "winter"
+      "season": "winter",
+      "price":"1000",
     },
     {
       "imgUrl": "Images/diz-38-768x768.jpg",
       "name": "diz-38-768x768",
-      "season": "autumn"
+      "season": "autumn",
+      "price":"600",
     },
     {
       "imgUrl": "Images/diz-39-768x768.jpg",
       "name": "diz-39-768x768",
-      "season": "autumn"
+      "season": "autumn",
+      "price":"700",
     },
     {
       "imgUrl": "Images/diz-41-768x768.jpg",
       "name": "diz-41-768x768",
-      "season": "spring"
+      "season": "spring",
+      "price":"800",
     },
     {
       "imgUrl": "Images/diz-42-768x768.jpg",
       "name": "diz-42-768x768",
-      "season": "autumn"
+      "season": "autumn",
+      "price":"900",
     },
     {
       "imgUrl": "Images/diz-43-768x768.jpg",
       "name": "diz-43-768x768",
-      "season": "winter"
+      "season": "winter",
+      "price":"1200",
     },
     {
       "imgUrl": "Images/diz-44-768x768.jpg",
       "name": "diz-44-768x768",
-      "season": "autumn"
+      "season": "autumn" ,
+      "price":"800",
     },
     {
       "imgUrl": "Images/diz-46-768x768.jpg",
       "name": "diz-46-768x768",
-      "season": "summer"
+      "season": "summer", 
+      "price":"1000",
     },
     {
       "imgUrl": "Images/diz-47-768x768.jpg",
       "name": "diz-47-768x768",
-      "season": "spring"
+      "season": "spring", 
+      "price":"1200",
     },
     {
       "imgUrl": "Images/diz-52-768x768.jpg",
       "name": "diz-52-768x768",
-      "season": "winter"
+      "season": "winter" ,
+      "price":"1250",
     },
     {
       "imgUrl": "Images/diz-54-768x768.jpg",
       "name": "diz-54-768x768",
-      "season": "autumn"
+      "season": "autumn", 
+      "price":"1300",
     },
     {
       "imgUrl": "Images/diz-28-768x768.jpg",
       "name": "diz-28-768x768",
-      "season": "winter"
+      "season": "winter", 
+      "price":"1200",
     },
     {
       "imgUrl": "Images/diz-18-768x768.jpg",
       "name": "diz-18-768x768",
-      "season": "summer"
+      "season": "summer" ,
+      "price":"1300",
     },
     {
       "imgUrl": "Images/diz-1-768x768.jpg",
       "name": "diz-1-768x768",
-      "season": "spring"
+      "season": "spring", 
+      "price":"1400",
     },
     {
       "imgUrl": "Images/diz-30-1-768x768.jpg",
       "name": "diz-30-1-768x768",
-      "season": "autumn"
+      "season": "autumn", 
+      "price":"800",
     },
     {
       "imgUrl": "Images/diz-34-768x768.jpg",
       "name": "diz-34-768x768",
-      "season": "winter"
+      "season": "winter" ,
+      "price":"1450",
     },
     {
       "imgUrl": "Images/diz-36-768x768.jpg",
       "name": "diz-36-768x768",
-      "season": "summer"
+      "season": "summer" ,
+      "price":"1050",
     },
     {
       "imgUrl": "Images/diz-7-768x768.jpg",
       "name": "diz-7-768x768",
-      "season": "spring"
+      "season": "spring", 
+      "price":"1100",
     },
     {
       "imgUrl": "Images/diz-4-768x768.jpg",
       "name": "diz-4-768x768",
-      "season": "autumn"
+      "season": "autumn" ,
+      "price":"1550",
     },
     {
       "imgUrl": "Images/diz-2-768x768.jpg",
       "name": "diz-2-768x768",
-      "season": "autumn"
+      "season": "autumn" ,
+      "price":"1600",
     },
     {
       "imgUrl": "Images/diz-7-768x768.jpg",
       "name": "diz-7-768x768",
-      "season": "autumn"
+      "season": "autumn" ,
+      "price":"1350",
     },
     {
       "imgUrl": "Images/diz-28-768x768.jpg",
       "name": "diz-28-768x768",
-      "season": "winter"
+      "season": "winter" ,
+      "price":"890",
     },
     {
       "imgUrl": "Images/diz-48-768x768.jpg",
       "name": "diz-48-768x768",
-      "season": "winter"
+      "season": "winter" ,
+      "price":"770",
     },
     {
       "imgUrl": "Images/diz-49-768x768.jpg",
       "name": "diz-49-768x768",
-      "season": "spring"
+      "season": "spring" ,
+      "price":"560",
     },
     {
       "imgUrl": "Images/diz-50-768x768.jpg",
       "name": "diz-50-768x768",
-      "season": "summer"
+      "season": "summer", 
+      "price":"990",
     },
     {
       "imgUrl": "Images/diz-51-768x768.jpg",
       "name": "diz-51-768x768",
-      "season": "spring"
+      "season": "spring",
+      "price":"570",
     },
     {
         "imgUrl": "Images/diz-55-768x768.jpg",
         "name": "diz-55-768x768",
-        "season": "summer"
+        "season": "summer",
+        "price":"470",
       },
       {
         "imgUrl": "Images/diz-56-768x768.jpg",
         "name": "diz-56-768x768",
-        "season": "winter"
+        "season": "winter",
+        "price":"980",
       },
       {
         "imgUrl": "Images/diz-53-768x768.jpg",
         "name": "diz-53-768x768",
-        "season": "summer"
+        "season": "summer",
+        "price":"970",
       },
       {
         "imgUrl": "Images/diz-57-768x768.jpg",
         "name": "diz-57-768x768",
-        "season": "spring"
+        "season": "spring",
+        "price":"780",
       },
       {
         "imgUrl": "Images/diz-58-768x768.jpg",
         "name": "diz-58-768x768",
-        "season": "autumn"
+        "season": "autumn",
+        "price":"1060",
       },
       {
         "imgUrl": "Images/diz-59-768x768.jpg",
         "name": "diz-59-768x768",
-        "season": "summer"
+        "season": "summer",
+        "price":"765",
       },
       {
         "imgUrl": "Images/diz-60-768x768.jpg",
         "name": "diz-60-768x768",
-        "season": "spring"
+        "season": "spring",
+        "price":"860",
       },
       {
         "imgUrl": "Images/diz-62-768x768.jpg",
         "name": "diz-62-768x768",
-        "season": "autumn"
+        "season": "autumn",
+        "price":"780",
       },
       {
         "imgUrl": "Images/diz-63-768x768.jpg",
         "name": "diz-63-768x768",
-        "season": "spring"
+        "season": "spring",
+        "price":"1270",
       },
       {
         "imgUrl": "Images/diz-65-768x768.jpg",
         "name": "diz-65-768x768",
-        "season": "winter"
+        "season": "winter",
+        "price":"670",
       },
       {
         "imgUrl": "Images/diz-66-768x768.jpg",
         "name": "diz-66-768x768",
-        "season": "spring"
+        "season": "spring",
+        "price":"570",
       },
       {
         "imgUrl": "Images/diz-67-768x768.jpg",
         "name": "diz-67-768x768",
-        "season": "autumn"
+        "season": "autumn",
+        "price":"870",
       },
       {
         "imgUrl": "Images/diz-68-768x768.jpg",
         "name": "diz-68-768x768",
-        "season": "summer"
+        "season": "summer",
+        "price":"770",
       },
       {
         "imgUrl": "Images/diz-73-768x768.jpg",
         "name": "diz-73-768x768",
-        "season": "winter"
+        "season": "winter",
+        "price":"770",
       },
       {
         "imgUrl": "Images/diz-72-768x768.jpg",
         "name": "diz-72-768x768",
-        "season": "summer"
+        "season": "summer",
+        "price":"720",
       },
       {
         "imgUrl": "Images/diz-71-768x768.jpg",
         "name": "diz-71-768x768",
-        "season": "spring"
+        "season": "spring",
+        "price":"820",
       },
       {
         "imgUrl": "Images/diz-76-768x768.jpg",
         "name": "diz-76-768x768",
-        "season": "autumn"
+        "season": "autumn",
+        "price":"960",
       }];
     
       
